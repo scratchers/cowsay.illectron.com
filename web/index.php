c4d963a0e0a7912cdb2b8831127a8e623c464b07
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-05 02:57:11 +0000

    add plain route

--- web/index.php
+++ web/index.php
@@ -21,6 +21,17 @@ $app->get('/heroku', function() use($app) {
 });
 
 $app->get('/', function() use($app) {
+    return cowspell();
+});
+
+$app->get('/plain', function() use($app) {
+    return cowspell($plain = true);
+});
+
+$app->run();
+
+function cowspell(bool $plain = false) : string
+{
     $json = file_get_contents('https://raw.githubusercontent.com/duckduckgo/zeroclickinfo-goodies/master/share/goodie/cheat_sheets/json/harry-potter-spells.json');
     $data = json_decode($json, $array = true);
 
@@ -29,7 +40,11 @@ $app->get('/', function() use($app) {
     $spell = $data['sections'][$section][$index]['key'];
     $value = $data['sections'][$section][$index]['val'];
 
-    return "<pre>".\Cowsayphp\Cow::say($spell.PHP_EOL.$value)."</pre>";
-});
+    $cowspell = \Cowsayphp\Cow::say($spell.PHP_EOL.$value);
 
-$app->run();
+    if ($plain) {
+        return $cowspell;
+    }
+
+    return "<pre>$cowspell</pre>";
+}
